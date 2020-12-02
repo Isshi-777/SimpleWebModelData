4103be8ef8dbfa90b83fc26746869c8f184c374e
Isshi-777 <Isshi@777>
2020-12-03 00:24:19 +0900

    [add] 単純なデータのサンプルを作成

---
+++ Assets/SimpleWebModelData/Sample/Scripts/SimpleData/SimpleDataScene.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using UnityEngine;
+
+/// <summary>
+/// 単純なデータの挙動テスト
+/// </summary>
+public class SimpleDataScene : MonoBehaviour
+{
+    private void Start()
+    {
+        // ユーザーデータ
+        TextAsset userJson = Resources.Load<TextAsset>("SimpleData/UserJson");
+        SimpleData_User userData = JsonConvert.DeserializeObject<SimpleData_User>(userJson.text);
+
+        // アイテムデータ
+        TextAsset itemJson = Resources.Load<TextAsset>("SimpleData/ItemJson");
+        SimpleData_Item itemData = JsonConvert.DeserializeObject<SimpleData_Item>(itemJson.text);
+
+        Debug.Log(userData + " : " + itemData);
+    }
+}
