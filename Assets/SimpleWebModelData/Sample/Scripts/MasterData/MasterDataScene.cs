27de000f4a967a5bf92d3cd023400c83b344ab47
Isshi-777 <Isshi@777>
2020-12-05 13:06:39 +0900

    [add] マスターデータのサンプルを作成

---
+++ Assets/SimpleWebModelData/Sample/Scripts/MasterData/MasterDataScene.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using UnityEngine;
+
+/// <summary>
+/// マスターデータの挙動テスト
+/// </summary>
+public class MasterDataScene : MonoBehaviour
+{
+    private void Start()
+    {
+        // アイテムマスターデータ
+        MasterDataTable_Item itemTable = new MasterDataTable_Item();
+        string url = "MasterData/" + itemTable.TableName;
+        TextAsset json = Resources.Load<TextAsset>(url);
+
+        MasterDataTable_Item itemtTable2 = JsonConvert.DeserializeObject<MasterDataTable_Item>(json.text);
+        itemTable.UpdateDataList(json.text);
+
+        // クエストマスターデータ
+        MasterDataTable_Quest questTable = new MasterDataTable_Quest();
+        string url2 = "MasterData/" + questTable.TableName;
+        TextAsset json2 = Resources.Load<TextAsset>(url2);
+
+        MasterDataTable_Quest questTable2 = JsonConvert.DeserializeObject<MasterDataTable_Quest>(json2.text);
+        questTable.UpdateDataList(json2.text);
+
+        // 簡単なクエストのみ取得
+        var easyQuestList = questTable.GetEasyQuestList();
+
+        Debug.Log("complete !!!");
+    }
+}
---
+++ Assets/SimpleWebModelData/Sample/Scripts/MasterData/MasterDataTable_Quest.cs
@@ -0,0 +1,19 @@
+﻿/// <summary>
+/// クエストのマスターデータテーブル
+/// </summary>
+public class MasterDataTable_Quest : AMasterDataTable<MasterData_Quest>
+{
+    /// <summary>
+    /// テーブル名
+    /// </summary>
+    public override string TableName => "Mst_Quest";
+
+    /// <summary>
+    /// 簡単なリストを返す（配列）
+    /// </summary>
+    /// <returns></returns>
+    public MasterData_Quest[] GetEasyQuestList()
+    {
+        return this.dataList.FindAll(x => x.RecommendLevel < 20).ToArray();
+    }
+}
