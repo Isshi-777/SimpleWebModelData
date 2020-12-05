e5c1a7f4f59f4283d376840ef97d06a0fff98f12
Isshi-777 <Isshi@777>
2020-12-06 07:32:46 +0900

    [add] サンプル用のシングルトンクラスを作成

---
+++ Assets/SimpleWebModelData/Sample/SimpleSingleton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+
+public abstract class SimpleSingleton<T> where T : class, new()
+{
+    private static T instance = new T();
+
+    protected SimpleSingleton()
+    {
+        Debug.Assert(null == instance);
+    }
+
+    public static T GetInstance()
+    {
+        if (instance == null)
+        {
+            Debug.LogError(" instance is null ! type -> " + typeof(T).ToString());
+            return null;
+        }
+
+        return instance;
+    }
+}
